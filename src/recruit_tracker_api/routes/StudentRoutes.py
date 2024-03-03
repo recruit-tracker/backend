0a1456799befb3b764164298f930ed45350c1dc0
Jayden Pyles <jpylesbusiness@gmail.com>
2024-03-02 18:42:14 -0600

    wip: edit api

--- src/recruit_tracker_api/routes/StudentRoutes.py
+++ src/recruit_tracker_api/routes/StudentRoutes.py
@@ -1,13 +1,11 @@
 # PDM
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import JSONResponse
-from fastapi import HTTPException
-
+from gridfs import GridFS
 from openai import OpenAI
 from pymongo import errors
 
 import recruit_tracker_api.utils as utils
-
 from recruit_tracker_api.constants import MONGO_URL as url
 from recruit_tracker_api.constants import OPENAI_API_KEY
 from recruit_tracker_api.mongo import init_mongo
@@ -30,6 +28,11 @@ async def read(request: Request):
 
         result = user_collection.find(filter_conditions)
         result_list = list(result)
+
+        for user in result_list:
+            resume_hash = user.get("resume_hash")
+            pdf = db.fs.pdfs.find_one({"metadata.hash": resume_hash})
+            user["resume"] = pdf
 
         client.close()
 
@@ -55,6 +58,12 @@ async def create(request: Request):
         hash_pw = utils.hash_password(user.get("password"))
         user["password"] = hash_pw
 
+        pdf = user.get("resume")
+
+        if pdf:
+            file_hash = utils.store_pdf(db, pdf)  # store pdf in db
+            user["resume_hash"] = file_hash
+
         user_collection.insert_one({"_id": user.get("email"), **user})
 
         client.close()
@@ -66,8 +75,9 @@ async def create(request: Request):
             status_code=409,
         )
 
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+    # except Exception as e:
+    #     print(e)
+    #     return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
 @student_router.post("/student/update")
@@ -106,39 +116,43 @@ async def delete(request: Request):
         return JSONResponse(content={"error": str(e)}, status_code=200)
 
 
-
 @student_router.post("/student/login")
 async def login(request: Request):
     try:
         json_data = await request.json()
         user = json_data.get("user")
-        assert user and user.get("email") and user.get("password"), "Email and password are required."
+        assert (
+            user and user.get("email") and user.get("password")
+        ), "Email and password are required."
 
         client = init_mongo(url)
         db = client["recruit_tracker"]
         user_collection = db["users"]
 
         user_email = user.get("email")
-        
+
         result = user_collection.find_one({"email": user_email})
 
         if not result:
             raise HTTPException(status_code=401, detail="Email not found.")
 
         if not utils.verify_password(user.get("password"), result.get("password")):
-            raise HTTPException(status_code=401, detail="Password does not match email.")
+            raise HTTPException(
+                status_code=401, detail="Password does not match email."
+            )
 
         return JSONResponse(content={"login": "Login successful!"}, status_code=200)
 
     except HTTPException as he:
-        return JSONResponse(content={"login": str(he.detail)}, status_code=he.status_code)
+        return JSONResponse(
+            content={"login": str(he.detail)}, status_code=he.status_code
+        )
 
     except AssertionError as ae:
         return JSONResponse(content={"error": str(ae)}, status_code=400)
 
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
-
 
 
 @student_router.post("/api/testgpt")
--- src/recruit_tracker_api/utils.py
+++ src/recruit_tracker_api/utils.py
@@ -1,10 +1,27 @@
+import hashlib
+
 import bcrypt
+from bson.objectid import ObjectId
+from gridfs import GridFS
+from pymongo import MongoClient
 
 
 def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password.decode("utf-8")
+
 
 def verify_password(password: str, hashed_password: str) -> bool:
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
+
+
+def store_pdf(db, pdf):
+    fs = GridFS(db, collection="pdfs")  # Use a custom collection for PDFs
+
+    sha256 = hashlib.sha256()
+    sha256.update(pdf.encode())
+    file_hash = sha256.hexdigest()
+
+    fs.put(pdf.encode(), metadata={"hash": file_hash})
+    return file_hash
