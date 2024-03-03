4d0866cd86f7a3a7d78d9fc91ac6c7d6cd480ac3
Jayden Pyles <jpylesbusiness@gmail.com>
2024-03-02 20:02:28 -0600

    wip: add CORS

--- src/recruit_tracker_api/app.py
+++ src/recruit_tracker_api/app.py
@@ -3,5 +3,14 @@ from fastapi import FastAPI
 from recruit_tracker_api.routes import hr_router, student_router
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(hr_router)
 app.include_router(student_router)
