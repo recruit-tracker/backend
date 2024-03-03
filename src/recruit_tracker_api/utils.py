806c53830e22f52de94a41907e6971d23993d716
Josh <joshd18@uab.edu@autograder.cs.uab.edu>
2024-03-02 19:15:58 -0600

    decode role from token

--- src/recruit_tracker_api/utils.py
+++ src/recruit_tracker_api/utils.py
@@ -14,12 +14,6 @@ from recruit_tracker_api.constants import ALGORITHM, SECRET_KEY
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
-class Student:
-    username = str
-    ...
-
-
 def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
@@ -31,13 +25,15 @@ def verify_password(password: str, hashed_password: str) -> bool:
 
 
 def create_jwt_token(user: dict):
-    to_encode = user.copy()
-    to_encode["role"] = user.get("role")
+    to_encode = {
+        str(key): value if isinstance(value, (str, int, float, bool, type(None))) else str(value)
+        for key, value in user.items()
+    }
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(create_jwt_token)):
+def get_current_user(token: str = Depends(create_jwt_token)):
     credentials_exception = HTTPException(
         status_code=401,
         detail="Could not validate credentials",
@@ -45,39 +41,19 @@ async def get_current_user(token: str = Depends(create_jwt_token)):
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
+        username: str = payload.get("email")
         role: str = payload.get("role")  # Extract the "role" claim
         if username is None or role is None:
             raise credentials_exception
-        token_data = {"sub": username, "role": role}
+        token_data = {"email": username, "role": role}
     except jwt.PyJWTError:
         raise credentials_exception
     return token_data
 
 
-async def decode_role(current_user: dict = Depends(get_current_user)):
+def decode_role(current_user: dict = Depends(get_current_user)):
     return current_user.get("role")
 
-    # user = {
-    #     id: 1,
-    #     "name": "Mitchell Kimbell",
-    #     "email": "mfkimbell@gmail.com",
-    #     "password": "immfkingbell",
-    #     "phone": "2053128982",
-    #     "state": "AL",
-    #     "school": "University of Alabama at Birmingham",
-    #     "graduation": "05/2024",
-    #     "position": "Intern",
-    #     "officeLocation": "Birmingham",
-    #     "stage": "first-round",
-    #     "interest": 8,
-    #     "linkedIn": "https://www.linkedin.com/in/mitchell-kimbell-mscs-3384191a1/",
-    #     "feedback": "This person was annoying"
-    #   }
-
-    # tok = create_jwt_token(user)
-    # print(tok)
-    return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
 
 
 def store_pdf(db, pdf):
